ce6bd02576bc3e974acf57c9decf04e30626c695
Jeff Schomay <jschomay@gmail.com>
2018-03-25 12:08:24 -0700

    update .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.16.0"]
                   [lein-auto  "0.1.3"]
                   [lein-cljfmt "0.5.7"]
-                  [venantius/ultra  "0.5.2"]]}}
+                  [venantius/ultra  "0.5.2"]
+                  [lein-kibit  "0.1.6"]]
+        :dependencies [[cljfmt  "0.5.1"]]}}
